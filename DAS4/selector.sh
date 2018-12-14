b386edcdc9067f20d6ca11cb3b184636efbce5fd
Luc-Veldhuis <luc.veldhuis@nubisonline.nl>
2018-12-14 16:27:53 +0100

    Add timing

--- DAS4/selector.sh
+++ DAS4/selector.sh
@@ -7,10 +7,12 @@ else
     if [ "$(hostname)" != "node$2" ]; then
         echo workerNode
         cd .././WorkerNode/build/install/WorkerNode/bin
+        sleep 20
         ./WorkerNode akka.tcp://root-node@node$1:2552/user/*
     else
         echo clientNode
         cd .././Client/build/install/Client/bin
+        sleep 40
         ./Client akka.tcp://root-node@node$2:2552/user/*
     fi
 fi
