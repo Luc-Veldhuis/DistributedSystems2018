7a5858cdc9d4c5aa52e060d927ff409afdb6e8bd
Luc-Veldhuis <luc.veldhuis@nubisonline.nl>
2018-12-14 16:25:46 +0100

    Fix path

--- DAS4/selector.sh
+++ DAS4/selector.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [ "$(hostname)" == "node$1" ]; then
     echo headnode
-    cd ./HeadNode/build/install/HeadNode/bin
+    cd .././HeadNode/build/install/HeadNode/bin
     ./HeadNode
 else
     if [ "$(hostname)" != "node$2" ]; then
         echo workerNode
-        cd ./WorkerNode/build/install/WorkerNode/bin
+        cd .././WorkerNode/build/install/WorkerNode/bin
         ./WorkerNode akka.tcp://root-node@node$1:2552/user/*
     else
         echo clientNode
-        cd ./Client/build/install/Client/bin
+        cd .././Client/build/install/Client/bin
         ./Client akka.tcp://root-node@node$2:2552/user/*
     fi
 fi
