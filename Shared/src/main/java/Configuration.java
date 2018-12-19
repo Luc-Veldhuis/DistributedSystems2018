c5c9e7ca3b057eb6432d66b63a8b3bbb672b23fe
Luc Veldhuis <luc.veldhuis@nubisonline.nl>
2018-12-19 15:23:58 +0100

    Fix configuration error

--- Shared/src/main/java/Configuration.java
+++ Shared/src/main/java/Configuration.java
@@ -2,9 +2,9 @@ public class Configuration {
     /**
      * All global configuration parameters should be stored here
      */
-    public static final int NUMBER_OF_ERRORS_TO_CORRECT = 2;
-    public static final int NUMBER_OF_HEADNODES = NUMBER_OF_ERRORS_TO_CORRECT+1;
     public static final int NUMBER_OF_WORKERS_PER_SYSTEM = 8;
+    public static int NUMBER_OF_ERRORS_TO_CORRECT = 2;
+    public static int NUMBER_OF_HEADNODES = NUMBER_OF_ERRORS_TO_CORRECT+1;
     public static final int MAXIMUM_FAKED_EXECUTION_TIME = 1000;
     public static final int TIMEOUT_DETECTION_TIME = 5000;
     public static final int NUMBER_OF_CONCURRENT_JOBS = 80;
@@ -23,7 +23,8 @@ public class Configuration {
 
     Configuration(String[] args) {
         if(args.length == 5) {
-            NUMBER_OF_DUPLICATIONS = Integer.parseInt(args[0]);
+            NUMBER_OF_ERRORS_TO_CORRECT = Integer.parseInt(args[0]);
+            NUMBER_OF_DUPLICATIONS = NUMBER_OF_ERRORS_TO_CORRECT*2+1;
             RATE_OF_STOP_FAILURES = Double.parseDouble(args[1]);
             RATE_OF_SILENT_FAILURES = Double.parseDouble(args[2]);
             RATE_OF_BYZANTINE_FAILURES = Double.parseDouble(args[3]);
