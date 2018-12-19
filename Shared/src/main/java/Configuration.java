302fda38cecabb89d8958459e1412b7e6da592ad
Luc-Veldhuis <Luc-Veldhuis@users.noreply.github.com>
2018-12-19 22:01:36 +0100

    Fix faked executiong time

--- Shared/src/main/java/Configuration.java
+++ Shared/src/main/java/Configuration.java
@@ -5,7 +5,7 @@ public class Configuration {
     public static final int NUMBER_OF_WORKERS_PER_SYSTEM = 50;
     public static int NUMBER_OF_ERRORS_TO_CORRECT = 2;
     public static int NUMBER_OF_HEADNODES = NUMBER_OF_ERRORS_TO_CORRECT+1;
-    public static final int MAXIMUM_FAKED_EXECUTION_TIME = 1000;
+    public static final int MAXIMUM_FAKED_EXECUTION_TIME = 8000;
     public static final int TIMEOUT_DETECTION_TIME = 5000;
     public static int NUMBER_OF_CONCURRENT_JOBS = 80;
 
