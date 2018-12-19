790238ae2b95d1aa33a9f2baf3f144227433c37e
Luc Veldhuis <luc.veldhuis@nubisonline.nl>
2018-12-19 14:39:39 +0100

    Hopefully fix it

--- DAS4/scriptToSubmitToScheduler.sh
+++ DAS4/scriptToSubmitToScheduler.sh
@@ -4,4 +4,9 @@
 ids=$(echo $SLURM_JOB_NODELIST | grep -oP "(\d*)")
 echo $ids
 echo "starting with options $1 $2 $3 $4 $5"
+cd ../HeadNode
+python replacer.py node$ids
+cd ../DAS4
+gradle build
+gradle install
 srun -N10 -l ./selector.sh $1 $2 $3 $4 $5 $ids
--- DAS4/selector.sh
+++ DAS4/selector.sh
@@ -3,10 +3,6 @@ echo $6 $7
 if [ "$(hostname)" == "node$6" ]; then
     echo node$6
     echo headnode
-    python replacer.py node$6
-    cd ../HeadNode
-    gradle build
-    gradle install
     cd .././HeadNode/build/install/HeadNode/bin
     ./HeadNode $1 $2 $3 $4 $5
     echo "Headnode is done"
@@ -16,11 +12,6 @@ else
         cd .././WorkerNode/build/install/WorkerNode/bin
         sleep 30
         echo FirstSleep
-        sleep 30
-        echo SecondSleep
-        sleep 30
-        echo ThirdSleep
-        sleep 30
         ./WorkerNode akka.tcp://root-node@node$6:2552/user/*
         echo "Worker is done"
     else
@@ -28,14 +19,8 @@ else
         cd .././Client/build/install/Client/bin
         sleep 30
         echo FirstSleep Client
-        sleep 30
-        echo SecondSleep Client
-        sleep 30
-        echo ThirdSleep Client
-        sleep 30
-        echo FourthSleep Client
-        sleep 5
-        echo FifthSleep
+        sleep 10
+        echo SecondSleep
         #sleep 30
         #echo SixthSleep
 
